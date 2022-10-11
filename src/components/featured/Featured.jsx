620587866fdfcc130cfe30bd96a1a84cb0d244d6
Mauricio Oliveira <mauricio.oliveira8583@gmail.com>
2022-10-11 16:42:11 -0300

    Finishing the intro section and starting the featured section

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -4,7 +4,28 @@ import './featured.scss'
 const Featured = () => {
   return (
     <div className='featured' id='featured'>
-        Featured
+        <h1>Portfolio</h1>
+        <ul>
+          <li className='active'>Featured</li>
+          <li>Store App</li>
+          <li>E-Sports</li>
+          <li>Restaurant App</li>
+        </ul>
+        <div className='appsContainer'>
+          <div className='appsContainerItem'>
+             <img src='../../images/site_1.png' alt='' />
+             <h3>Pizza Delivery App</h3>            
+          </div>
+          <div className='appsContainerItem'>
+             <img src='../../images/site_1.png' alt='' />
+             <h3>Pizza Delivery App</h3>            
+          </div>
+          <div className='appsContainerItem'>
+             <img src='../../images/site_1.png' alt='' />
+             <h3>Pizza Delivery App</h3>            
+          </div>
+          
+        </div>
     </div>
   )
 }
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,25 @@
+import { ArrowDownward } from '@material-ui/icons'
 import React from 'react'
 import './intro.scss'
 
 const Intro = () => {
   return (
     <div className='intro' id='intro'>
-        Intro
+        <div className='left'>
+          <div className='imgContainer'>
+            <img src='../../images/perfil.jpg' alt='Perfil' />
+          </div>
+        </div>
+        <div className='right'>
+          <div className='wrapper'>
+            <h2>Desenvolvedor Web</h2>
+            <h1>Mauricio Oliveira</h1>
+            <h3>FrontEnd</h3>
+          </div>
+          <a href='#featured'>
+            <ArrowDownward className='arrowDown' />
+          </a>
+        </div>
     </div>
   )
 }
